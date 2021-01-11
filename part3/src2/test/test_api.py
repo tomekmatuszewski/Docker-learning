10ac18d11490dfc7df8a0031cc7fbf458ae26f7d
TomekM <tomasz_matuszewski@o2.pl>
2021-01-11 20:07:20 +0100

    new-task added

---
+++ part3/src2/test/test_api.py
@@ -0,0 +1,20 @@
+import requests
+import random
+import os
+
+base_uri = os.getenv('API_HOST', 'http://localhost')
+port = os.getenv('API_PORT', 3000)
+posts_uri = f'{base_uri}:{port}/posts'
+
+
+def test_api_get_posts():
+	r = requests.get(posts_uri)
+	assert r.status_code == 200
+
+def test_creating_post():
+	marker = random.randint(1, 100000)
+	body = {'title': 'test post', 'author': 'ag'}
+	r = requests.post(posts_uri, json=body)
+	assert r.status_code == 201
+
+
